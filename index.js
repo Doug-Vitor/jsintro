a4db0984201190c9c59aae31e53599c8acbdbf16
Douglas <douglasvitor.dev@gmail.com>
2022-06-20 09:31:14 -0300

    Section 1.5: Event listeners

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Anonymous functions.
+/*// Anonymous functions.
 console.log((function (number1, number2, operator) {
     return eval(`${number1} ${operator} ${number2}`)
 })(1, 2, "*"));
@@ -8,4 +8,14 @@ let result = (number1, number2, operator) => {
     return eval(`${number1} ${operator} ${number2}`)
 }
 
-console.log(result(10, 20, "*"));
+console.log(result(10, 20, "*"));*/
+
+// Event listener to the window.
+window.addEventListener('focus', () => {
+    console.log('focus')
+})
+
+// Event listener to the document.
+document.addEventListener('click', () => {
+    console.log('click')
+})
